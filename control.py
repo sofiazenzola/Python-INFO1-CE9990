a5a13785018404f2110574e47ca5cdd62bfdf5e0
sofiazenzola <zenzolas@gmail.com>
2017-06-27 09:58:08 -0400

    Create control.py

---
+++ control.py
@@ -0,0 +1,23 @@
+"""
+control.py
+Ask the user for input if it is cloudy outside
+Outputs some advice for the weather
+
+
+"""
+
+import sys
+
+while True:
+    answer= input("Is it cloudy outside?")
+
+    if answer.lower()=="yes":
+        print("You may need this ☂")
+
+    elif answer.lower()=="no":
+        print("Enjoy the sun ☀")
+    else:
+        print("Please type only ""yes"" or ""no""")
+
+
+sys.exit(0)
